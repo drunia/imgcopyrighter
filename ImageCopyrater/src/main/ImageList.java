54774548831b380c4a663da7bae7cbdf4c6bb5f3
drunia <drunia@192.168.1.2>
2013-04-09 00:14:26 +0300

    hgy

--- ImageCopyrater/src/main/ImageList.java
+++ ImageCopyrater/src/main/ImageList.java
@@ -11,7 +11,7 @@ import javax.swing.JLabel;
 import javax.swing.JList;
 
 public class ImageList extends JList<JLabel> {
-	private static final long serialVersionUID = 146L;
+	private static final long serialVersionUID = 14699L;
 
 	/**
 	 * Default constructor
