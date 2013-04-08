3f189480e49e1a97cea3965f7d2aa10c7cd9db30
drunia <drunia@amd>
2013-04-08 22:19:51 +0300

    First commit

---
+++ ImageCopyrater/src/main/ImageCopyrighter.java
@@ -0,0 +1,72 @@
+package main;
+
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.Graphics;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
+import javax.swing.BoxLayout;
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+
+
+public class ImageCopyrighter extends JFrame {
+	private JButton startButton;
+	private JTextField textField;
+	private ImageList imgList;
+	private static final long serialVersionUID = 1453023172562886029L;
+	private Font font;
+	
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		ImageCopyrighter mf = new ImageCopyrighter();
+		mf.setVisible(true);
+		File[] files = {new File("D:/1.jpg"), new File("d:/DSC_0034.JPG")};
+		mf.doIt(files);
+		
+	}
+	
+	public ImageCopyrighter() {
+		super("Copyright creator by drunia");
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setSize(600, 400);
+		setLocationRelativeTo(null);
+	
+		imgList = new ImageList();
+		add(imgList);
+		
+		JPanel cPanel = new JPanel(); 
+		cPanel.setLayout(new BoxLayout(cPanel, BoxLayout.Y_AXIS));
+	}
+	
+	/**
+	 * Main method
+	 * @param files - array of selected file/s
+	 */
+	public void doIt(File[] files) {
+		imgList.setElements(files);
+		for (int i = 0; i < files.length; i++) {
+			
+		}
+	}
+	
+	/**
+	 * Draw text method
+	 * @param g
+	 */
+	private void drawCopyRight(Graphics g) {
+		g.setColor(Color.RED);
+		g.drawString(textField.getText(), 0, 20);
+	}
+
+}
---
+++ ImageCopyrater/src/main/ImageList.java
@@ -0,0 +1,56 @@
+package main;
+
+import java.awt.Color;
+import java.awt.Component;
+import java.awt.Image;
+import java.io.File;
+
+import javax.swing.BorderFactory;
+import javax.swing.DefaultListCellRenderer;
+import javax.swing.ImageIcon;
+import javax.swing.JLabel;
+import javax.swing.JList;
+
+public class ImageList extends JList {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * Default constructor
+	 */
+	public ImageList() {
+		super();
+		setCellRenderer(new ILCellRenderer());
+	}
+	
+	/**
+	 * Set elements to list from File[] array
+	 * @param files
+	 */
+	public void setElements(File[] files) {
+		JLabel[] elements = new JLabel[files.length];
+		for (int i = 0; i < elements.length; i++) {
+			elements[i] = new JLabel(files[i].getAbsolutePath());
+		}
+		setListData(elements);
+	}
+}
+
+/**
+ * Image list cell renderer  class
+ * @author drunia
+ */
+class ILCellRenderer extends DefaultListCellRenderer {
+	private static final long serialVersionUID = 1L;
+	
+	@Override
+	public Component getListCellRendererComponent(JList list, Object value,
+			int index, boolean isSelected, boolean cellHasFocus) {
+		JLabel listElement = (JLabel) value;
+		ImageIcon icon = new ImageIcon(listElement.getText());
+		icon = new ImageIcon(icon.getImage().getScaledInstance(48, -1, Image.SCALE_DEFAULT));
+		listElement.setIcon(icon);
+		listElement.setToolTipText(listElement.getText());
+		listElement.setBorder(BorderFactory.createRaisedBevelBorder());
+		return listElement;
+	}
+}
