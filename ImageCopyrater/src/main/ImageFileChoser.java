fc9b744b15395d4f0562caae2e4c4edacc2463f8
drunia <drunia@192.168.1.2>
2013-04-10 00:33:21 +0300

    h

--- ImageCopyrater/src/main/ImageFileChoser.java
+++ ImageCopyrater/src/main/ImageFileChoser.java
@@ -1,7 +1,54 @@
 package main;
 
-import javax.swing.JFileChooser;;
+import java.io.File;
+
+import javax.swing.JFileChooser;
+import javax.swing.filechooser.FileFilter;
 
 public class ImageFileChoser extends JFileChooser {
+	/**
+	 * For serialization
+	 */
+	private static final long serialVersionUID = -2799438855523865110L;
+	
+	/**
+	 * Array of assigned extensions
+	 */
+	private String[] exts = {"jpg", "jpeg", "png"};
+	
+	/**
+	 * Class - filter for image extensions
+	 * @author drunia
+	 */
+	private class ImageFilter extends FileFilter {
+		/**
+		 * Check compatible files
+		 */
+		@Override
+		public boolean accept(File f) {
+			for (int i = 0; i < exts.length; i++) {
+				if (f.getName().toLowerCase().endsWith(exts[i])) 
+					return true;
+			}
+			return false;
+		}
+
+		/**
+		 * Return ext description
+		 */
+		@Override
+		public String getDescription() {
+			// TODO Auto-generated method stub
+			return "ImageCopyrighter files";
+		}
+	}
+	
+	/**
+	 * Default constructor
+	 */
+	public ImageFileChoser() {
+		super();
+		setFileFilter(new ImageFilter());
+	}
 
 }
