83bca3d83ceaefcaaad176a8b960616513dbe3d7
drunia <druni@@SHAITAN-MACHINE>
2013-04-09 12:10:17 +0300

    wc4

--- ImageCopyrater/src/main/ImgConfiguration.java
+++ ImageCopyrater/src/main/ImgConfiguration.java
@@ -5,6 +5,8 @@ package main;
 
 import java.awt.Dimension;
 import java.awt.Font;
+import java.awt.FontMetrics;
+import java.awt.Graphics;
 import java.awt.Image;
 
 import javax.swing.ImageIcon;
@@ -15,9 +17,10 @@ import javax.swing.ImageIcon;
  */
 public class ImgConfiguration {
 	private Font font;
-	private Dimension orientation;
+	private int orientation;
 	private String text;
 	private Image img;
+	private boolean logo;
 	
 	public static final int ORIENTATION_TOPLEFT      = 0;
 	public static final int ORIENTATION_TOPCENTER    = 1;
@@ -29,9 +32,12 @@ public class ImgConfiguration {
 	
 	/**
 	 * Default constructor
+	 * @param logo - Image
+	 * @param orientationmn - ImgConfiguration.ORIENTATION_XXXXXXXXX
 	 */
-	public ImgConfiguration() {
+	public ImgConfiguration(Image logo, int orientation) {
 		super();
+		this.logo = (logo == null) ? false : true; 
 	}
 	
 	/**
@@ -72,8 +78,33 @@ public class ImgConfiguration {
 	 * Calculate text & logo orientation
 	 * @return Dimension (X, Y) position
 	 */
-	public Dimension getOrientation() {
-		return orientation;
+	public Dimension getOrientation(Graphics g) {
+		FontMetrics fm = g.getFontMetrics(font);
+		int textWidth = fm.stringWidth(text);
+		int textHeight = fm.getHeight();
+		int x = 0, y = 0;
+		
+		//get Y text center by logo
+		if (logo) {
+			textWidth += img.getWidth(null) + 10; // 10 px space logo<->text
+			textHeight = (int) (img.getHeight(null) / 2) - (textHeight / 2);
+		}
+		
+		//Calculate orientation
+		switch (orientation) {
+			case ORIENTATION_TOPLEFT:
+				x = 0;
+				y = 0;
+				break;
+			case ORIENTATION_TOPCENTER:
+				x = (int) (g.getClipBounds().width / 2) - (textWidth / 2); 
+				y = 0;
+			case ORIENTATION_TOPRIGHT:
+				x = g.getClipBounds().width - textWidth;
+				y = 0;
+		}
+		
+		return new Dimension(x, y);
 	}
 
 	/**
@@ -85,6 +116,10 @@ public class ImgConfiguration {
 		return text;
 	}
 
+	/**
+	 * Return copyright logo
+	 * @return Image
+	 */
 	public Image getImg() {
 		if (img == null) 
 			img = new ImageIcon(getClass().getResource("/res/def_logo.png")).getImage();
